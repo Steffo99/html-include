8ebd8f6a6f9e0bbc5c97ff8f5204ce81bbe1eca2
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-11-05 21:46:17 +0100

    Remove logs

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function replace(element) {
     if(!element.children) return;
 	for (var n in element.children) {
-        console.info("Checking " + element.tagName + ".")
 		if (element.children[n].hasAttribute && element.children[n].hasAttribute('data-include')) {
             loadIn(element.children[n], element.children[n].getAttribute('data-include'));
 		}
@@ -10,7 +9,6 @@ function replace(element) {
 }
 
 function loadIn(element, url) {
-    console.info("Loading \"" + url + "\" .");
     fetch(url).done(function(response) {
         response.text().done(function(text) {
             element.innerHTML = text;
